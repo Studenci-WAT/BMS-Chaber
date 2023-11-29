e0f56f1da5fb74419aa8fcff36e5fca8e42ffcf7
Klit3k <wojciech@klicki.xyz>
2023-11-29 20:51:39 +0100

    Adding entire package instead of single annotated class

--- src/main/java/pl/edu/wat/mspw_projekt/util/SessionFactoryMaker.java
+++ src/main/java/pl/edu/wat/mspw_projekt/util/SessionFactoryMaker.java
@@ -1,8 +1,15 @@
 package pl.edu.wat.mspw_projekt.util;
 
+import org.hibernate.MappingException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import pl.edu.wat.mspw_projekt.model.*;
+
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SessionFactoryMaker {
     private static SessionFactory factory;
@@ -10,7 +17,18 @@ public class SessionFactoryMaker {
     private static void configureFactory()
     {
         try {
-            factory = new Configuration()
+            factory = new WildCardConfiguration()
+                    .addPackage("pl.edu.wat.mspw_projekt.model")
+                    .configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    private static void configureFactoryTest()
+    {
+        try {
+            factory = new WildCardConfiguration()
                     .addAnnotatedClass(Ammo.class)
                     .addAnnotatedClass(AmmoCategory.class)
                     .addAnnotatedClass(GuidanceType.class)
@@ -22,13 +40,17 @@ public class SessionFactoryMaker {
             throw new ExceptionInInitializerError(ex);
         }
     }
-
-    public static org.hibernate.SessionFactory getFactory() {
+    public static SessionFactory getFactory() {
         if (factory == null) {
             configureFactory();
         }
-
         return factory;
     }
-
+    public static SessionFactory getFactoryTest() {
+        if (factory == null) {
+            configureFactoryTest();
+        }
+        return factory;
+    }
 }
+
--- src/test/java/pl/edu/wat/mspw_projekt/model/AmmoTest.java
+++ src/test/java/pl/edu/wat/mspw_projekt/model/AmmoTest.java
@@ -9,17 +9,26 @@ import pl.edu.wat.mspw_projekt.util.SessionFactoryMaker;
 class AmmoTest {
     @Test
     public void test1() {
-        Ammo ammo = new Ammo();
-        ammo.setName("Test1");
-        SessionFactory factory = SessionFactoryMaker.getFactory();
+        AmmoCategory ammoCategory = AmmoCategory.builder()
+                        .name("Test ammo category")
+                        .shortcut("TMC")
+                        .build();
+
+        Ammo ammo = Ammo.builder()
+                .name("Test2")
+                .category(ammoCategory)
+                .code("T")
+                .build();
+
+        SessionFactory factory = SessionFactoryMaker.getFactoryTest();
 
         try (Session session = factory.openSession()) {
             Transaction tx = session.beginTransaction();
+            session.save(ammoCategory);
             session.save(ammo);
             tx.commit();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-
 }
